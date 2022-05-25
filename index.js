a4fae006537aec67615266bb81ee01fd5c1eb569
pritom48 <pritamdas41148@gmail.com>
2022-05-25 10:47:19 +0600

    accessToken

--- index.js
+++ index.js
@@ -74,7 +74,8 @@ async function run() {
                 $set: user,
             };
             const result = await userCollection.updateOne(filter, updateDoc, options);
-            res.send(result)
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+            res.send({ result, token })
         })
 
     }
